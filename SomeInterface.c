a1ba32a0cfb03720e89ead7a2fb3a9103af77635
Springdl <1466134940@qq.com>
2022-08-03 22:57:53 +0800

    寄存器接口

--- SomeInterface.c
+++ SomeInterface.c
@@ -77,56 +77,56 @@ char *search_uid(int uid)
 void insert_param(Blocks *head)
 {
 }
-/**
- * @brief 获取所有全局变量
- *
- */
-void search_global_var()
-{
-    int i;
-    for (int i = symbolTable.index - 1; i >= 0; i--)
-    {
-        if (symbolTable.symbols[i].flag != FUNCTION && symbolTable.symbols[i].level == 0) //如果不是函数且层级为0，即是全局变量
-        {
-            //有可能是数组
-            struct symbol sym = symbolTable.symbols[i];
-            if (sym.flag == ARRAY || sym.flag == CONST_ARRAY) //可能是数组
-            {
-            }
-            else if (sym.flag == VAR || sym.flag == CONST_VAR) //全局变量 全局CONST变量
-            {
-                // char *alias = sym.alias;//别名，三地址代码中所有变量名都是别名
-                // int type = sym.type; //可能是TOK_INT TOK_FLOAT
-            }
-        }
-    }
-}
-/**
- * @brief 遍历数组的初值链表,是按顺序存的
- *
- * @param value_list
- */
-void traverse_List(List *value_list)
-{
-    void *element = NULL;
-    ListFirst(value_list, false); //重置iterator
-    while (ListNext(value_list, &element))
-    {
-        ArrayValue *value = (ArrayValue *)element;
-        if (value->kind == LITERAL) //整型常量
-        {
-        }
-        else if (value->kind == FLOAT_LITERAL) //浮点型常量
-        {
-        }
-        else if (value->kind == ID) //可以显式的写出来
-        {
-        }
-        else if (value->kind == NONE) //没法存在符号表里，比如说数组，所以我会显式的写出来
-        {
-        }
-    }
-}
+// /**
+//  * @brief 获取所有全局变量
+//  *
+//  */
+// void search_global_var()
+// {
+//     int i;
+//     for (int i = symbolTable.index - 1; i >= 0; i--)
+//     {
+//         if (symbolTable.symbols[i].flag != FUNCTION && symbolTable.symbols[i].level == 0) //如果不是函数且层级为0，即是全局变量
+//         {
+//             //有可能是数组
+//             struct symbol sym = symbolTable.symbols[i];
+//             if (sym.flag == ARRAY || sym.flag == CONST_ARRAY) //可能是数组
+//             {
+//             }
+//             else if (sym.flag == VAR || sym.flag == CONST_VAR) //全局变量 全局CONST变量
+//             {
+//                 // char *alias = sym.alias;//别名，三地址代码中所有变量名都是别名
+//                 // int type = sym.type; //可能是TOK_INT TOK_FLOAT
+//             }
+//         }
+//     }
+// }
+// /**
+//  * @brief 遍历数组的初值链表,是按顺序存的
+//  *
+//  * @param value_list
+//  */
+// void traverse_List(List *value_list)
+// {
+//     void *element = NULL;
+//     ListFirst(value_list, false); //重置iterator
+//     while (ListNext(value_list, &element))
+//     {
+//         ArrayValue *value = (ArrayValue *)element;
+//         if (value->kind == LITERAL) //整型常量
+//         {
+//         }
+//         else if (value->kind == FLOAT_LITERAL) //浮点型常量
+//         {
+//         }
+//         else if (value->kind == ID) //可以显式的写出来
+//         {
+//         }
+//         else if (value->kind == NONE) //没法存在符号表里，比如说数组，所以我会显式的写出来
+//         {
+//         }
+//     }
+// }
 /**
  * @brief 通过别名查找符号表，一般是在后端调用了，因为三地址代码中存储的变量的ID就是别名
  *
