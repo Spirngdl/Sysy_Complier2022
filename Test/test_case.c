49c59fb71bede714a2384bd9d28a931bb3249a06
酱紫编译的 <1466134940@qq.com>
2022-08-10 21:52:24 +0800

    d

--- Test/test_case.c
+++ Test/test_case.c
@@ -1,56 +1,65 @@
-void sort(int arr[], int len) {
-  int i = 0;
-  while (i < len - 1) {
-    int j = i + 1;
-    while (j < len) {
-      if (arr[i] < arr[j]) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
-      j = j + 1;
+int n;
+int swap (int array[], int i, int j){
+    int temp;
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    return 0;  
+}
+int heap_ajust(int arr[], int start, int end) {  
+    int dad;
+    dad = start;  
+    int son;
+    son = dad * 2 + 1;  
+    while (son < end + 1) { //   
+        if (son < end && arr[son] < arr[son + 1])
+            son = son + 1;  
+        if (arr[dad] > arr[son])
+            return 0;  
+        else {
+            dad = swap(arr,dad,son);  
+            dad = son;  
+            son = dad * 2 + 1;  
+        }  
+    }  
+    return 0;  
+}  
+int heap_sort(int arr[], int len) {  
+    int i;  
+    int tmp;
+    i = len / 2 - 1;
+    while ( i > -1) {
+        tmp = len - 1;
+        tmp = heap_ajust(arr, i, tmp);  
+        i = i - 1;
+    }    
+    i = len - 1;   
+    while ( i > 0) {  
+        int tmp0;
+        tmp0 = 0;
+        tmp = swap(arr,tmp0,i);
+        tmp = i - 1;
+        tmp = heap_ajust(arr, tmp0, tmp);  
+        i = i-1;
+    }  
+    return 0;
+}  
+
+int main(){
+    n = 10;
+    int a[10];
+    a[0]=4;a[1]=3;a[2]=9;a[3]=2;a[4]=0;
+    a[5]=1;a[6]=6;a[7]=5;a[8]=7;a[9]=8;
+    int i;
+    i = 0;
+    i = heap_sort(a, n);
+    while (i < n) {
+        int tmp;
+        tmp = a[i];
+        putint(tmp);
+        tmp = 10;
+        putch(tmp);
+        i = i + 1;
     }
-    i = i + 1;
-  }
-}
-
-
-int param32_arr(int a1[], int a2[], int a3[], int a4[], int a5[], int a6[],
-                int a7[], int a8[], int a9[], int a10[], int a11[], int a12[],
-                int a13[], int a14[], int a15[], int a16[], int a17[],
-                int a18[], int a19[], int a20[], int a21[], int a22[],
-                int a23[], int a24[], int a25[], int a26[], int a27[],
-                int a28[], int a29[], int a30[], int a31[], int a32[]) {
-
-  return 0;
-}
-
-int param16(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8,
-            int a9, int a10, int a11, int a12, int a13, int a14, int a15,
-            int a16) {
-  int arr[16] = {a1, a2,  a3,  a4,  a5,  a6,  a7,  a8,
-                 a9, a10, a11, a12, a13, a14, a15, a16};
-
-  return 0;
-}
-
-int main() {
-  int arr[32][2] = {{param16(getint(), getint(), getint(), getint(), getint(),
-                             getint(), getint(), getint(), getint(), getint(),
-                             getint(), getint(), getint(), getint(), getint(),
-                             getint()),
-                     8848}},
-      i = 1;
-  while (i < 32) {
-    arr[i][0] = arr[i - 1][1] - 1;
-    arr[i][1] = arr[i - 1][0] - 2;
-    i = i + 1;
-  }
-  putint(param32_arr(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6],
-                     arr[7], arr[8], arr[9], arr[10], arr[11], arr[12], arr[13],
-                     arr[14], arr[15], arr[16], arr[17], arr[18], arr[19],
-                     arr[20], arr[21], arr[22], arr[23], arr[24], arr[25],
-                     arr[26], arr[27], arr[28], arr[29], arr[30], arr[31]));
-  putch(10);
-  return 0;
+    return 0;
 }
